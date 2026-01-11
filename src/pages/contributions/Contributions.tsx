fd099c82f0324d916675cf9b77cb054ff4c7ecc1
Omolo <geowen74@yahoo.com>
2026-01-11 21:11:15 +0300

    Update Contributions.tsx

--- src/pages/contributions/Contributions.tsx
+++ src/pages/contributions/Contributions.tsx
@@ -8,6 +8,11 @@ export default function App() {
   );
 }
 
-export function Contributions() {
-  return <div>Contributions Component Works!</div>;
+// export function Contributions() {
+//   return <div>Contributions Component Works!</div>;
+// }
+
+// Temporary placeholder export
+export default function Contributions() {
+  return <div>Contributions is paused for testing.</div>;
 }
