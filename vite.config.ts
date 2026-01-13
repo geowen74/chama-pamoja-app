5fc65fa9684d39de1e726840ea025935e2dc2189
Omolo <geowen74@yahoo.com>
2026-01-14 00:40:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  // 👇 important so CSS/JS are loaded correctly when deployed
+  base: './',
+  server: {
+    port: 5173,
+    open: true,
+  },
+  preview: {
+    port: 4173,
   },
 })
